87993794cf2010cffdd61815818f03b6cb568330
Jigar A <jam@odoo.com>
2018-09-18 11:41:32 -0700

    [ADD] Added security module

---
+++ maqabim_extended_security/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Maqabim Distributors: Custom access rights",
+    'summary': "Web",
+    'description': """
+Maqabim Distributors: Custom access rights
+==========================================
+
+""",
+    "author": "Odoo Inc",
+    'website': "https://www.odoo.com",
+    'category': 'Custom Development',
+    'version': '0.1',
+    'depends': ['point_of_sale', 'maqabim_sale', 'stock', 'website_multi'],
+    'data': [
+        'security/security_groups.xml',
+        'security/ir.model.access.csv',
+        'views/res_users_views.xml',
+        'views/point_of_sale_views.xml',
+        'views/purchase_views.xml'
+    ],
+    'license': 'OEEL-1',
+}
