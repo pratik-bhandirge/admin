a23c47d2fb4e982edf4c9149c206812addcc5fbd
Pratik Bhandirge <pratik.bhandirge@bistasolutions.com>
2019-03-20 18:50:56 +0530

    [ADD] Date order in Bank reconciliation process

---
+++ maq_account/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'MAQ Accounting',
+    'summary': 'Customized and improve a Accounting features',
+    'category': 'Accounting',
+    'description': """
+Following accounting features will be added
+
+Req Id. 1.13 - Show records in ascending in Account Bank reconcilation Report
+    """,
+    'depends': ['account_reports'],
+    'data': [],
+    'qweb': [],
+    'auto_install': True,
+    'installable': True,
+    'license': 'OEEL-1',
+}
