146790d0aa26c532b5a90e8b4cae9486beff8c86
Jigar A. (Odoo) <jam@odoo.com>
2019-04-11 16:20:18 -0700

    [ADD] intenral user allowed website login

--- ecommerce_category/models/res_users.py
+++ ecommerce_category/models/res_users.py
@@ -12,6 +12,11 @@ _logger = logging.getLogger(__name__)
 class ResUsers(models.Model):
     _inherit = 'res.users'
 
+    login_website_ids = fields.Many2many(comodel_name='website', relation='user_website_login_check',
+                                            column1='user_id', column2='website_id', string='Website Allowed Login ',
+                                            help='This field must be used for intenral users authetication only and should not be used for portal users.')
+
+
     @classmethod
     def _login(cls, db, login, password):
         """
@@ -31,17 +36,32 @@ class ResUsers(models.Model):
             with cls.pool.cursor() as cr:
                 self = api.Environment(cr, SUPERUSER_ID, {})[cls._name]
                 user = self.search([('login', '=', login)])
-                # CUSTOM START
                 if user:
+                    # CUSTOM START
                     current_website_id = self.env['website'].get_current_website()
-                    if current_website_id.company_id.id not in user.company_ids.ids:
+                    if user and not user.share:
+                        if current_website_id not in users.login_website_ids:
+                            _logger.info('Multi-website login failed for db:%s login:%s website_id:%s', db, login, current_website_id)
+                            user = False
+                    else current_website_id.company_id.id not in user.company_ids.ids:
+                        _logger.info('Multi-website company login failed for db:%s login:%s website_id:%s', db, login, current_website_id)
                         user = False
-                # CUSTOM END
-                if user:
-                    user_id = user.id
-                    user.sudo(user_id).check_credentials(password)
-                    user.sudo(user_id)._update_last_login()
+
+                    # CUSTOM END
+                    if user and current_website_id:
+                        try:
+                            user_id = user.id
+                            user.sudo(user_id).check_credentials(password)
+                            user.sudo(user_id)._update_last_login()
+                        except AccessDenied:
+                            _logger.info('Multi-website login failed for db:%s login:%s website_id:%s', db, login, current_website_id)
+                            user_id = False
+                    else:
+                        user_id = False
+                else:
+                    user_id = False
         except AccessDenied:
+            _logger.info('login failed for db:%s login:%s', db, login)
             user_id = False
 
         status = "successful" if user_id else "failed"
