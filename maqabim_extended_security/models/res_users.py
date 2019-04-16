c0bc190aeaaf13037d1734b7588865f01bc53781
Keyur Gajjar <kga@odoo.com>
2019-04-16 09:44:01 -0700

    [ADD] added proper many2many replation tables

--- maqabim_extended_security/models/res_users.py
+++ maqabim_extended_security/models/res_users.py
@@ -7,5 +7,5 @@ from odoo import api, fields, models
 class ResUsers(models.Model):
     _inherit = "res.users"
 
-    pos_ids = fields.Many2many('pos.config', string='Allowed Pos')
-    warehouse_ids = fields.Many2many('stock.warehouse', string='Allowed Warehouses', domain="[('company_id', '=', company_id)]")
+    pos_ids = fields.Many2many('pos.config', 'res_users_pos_configuration_rel', 'user_id', 'pos_id', string='Allowed Pos')
+    warehouse_ids = fields.Many2many('stock.warehouse', 'res_user_warehouse_rel', 'user_id', 'warehouse_id', string='Allowed Warehouses', domain="[('company_id', '=', company_id)]")
