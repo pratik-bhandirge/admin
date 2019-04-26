f4885a368476b0ea6b104adeba8d2d39a10e3650
Pratik Bhandirge <pratik.bhandirge@bistasolutions.com>
2019-04-26 13:55:21 +0530

    [ADD] Rename customer reference field as external refernce as well as show it on delivery picking only

--- maq_sale_delivery_customer_ref/models/sale_order.py
+++ maq_sale_delivery_customer_ref/models/sale_order.py
@@ -7,7 +7,7 @@ class SaleOrder(models.Model):
 
     _inherit = 'sale.order'
 
-    m_customer_ref = fields.Char(string="Customer Reference", track_visibility='onchange')
+    m_customer_ref = fields.Char(string="External Reference", track_visibility='onchange')
 
 
 class StockPicking(models.Model):
@@ -15,4 +15,4 @@ class StockPicking(models.Model):
     _inherit = 'stock.picking'
 
     m_delivery_customer_ref = fields.Char(
-        related='sale_id.m_customer_ref', string="Customer Reference", track_visibility='onchange')
+        related='sale_id.m_customer_ref', string="External Reference", track_visibility='onchange')
