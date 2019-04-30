c6b765057454f928103a8f84967928961455fc3c
Pratik Bhandirge <pratik.bhandirge@bistasolutions.com>
2019-04-30 15:58:25 +0530

    [REF] Refactor  bista website module

--- bista_website_sale_options/models/product_product.py
+++
@@ -1,9 +0,0 @@
-
-# from odoo import models, fields, api
-# 
-# 
-# class ProductProduct(models.Model):
-#     
-#     _inherit = 'product.product'
-#     
-#     m_product_specification = fields.Html("Specification of the Product")
