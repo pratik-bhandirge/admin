94451c515748b0ab83522fe7bc6a8c0ada652af8
bista <jaykishan.sharma@bistasolutions.com>
2019-05-02 22:04:58 +0530

    [ADD] Added website_ids field for and is_website_publish boolean field to show products or message 'currently out of stock'. Added attrs for website_ids field

--- maq_prod_variant_unpublish/models/product.py
+++ maq_prod_variant_unpublish/models/product.py
@@ -6,22 +6,32 @@ from odoo import api, fields, models, _
 class ProductProduct(models.Model):
 
     _inherit = "product.product"
-#     is_website_publish = fields.Boolean(
-#         string="To show on website", default=True)
+    is_website_publish = fields.Boolean(
+        string="To show on website", default=True)
+#         partners = self.env.user.partner_id
+#         active_id = self._context.get('active_id')
+#         if self._context.get('active_model') == 'res.partner' and active_id:
+#             if active_id not in partners.ids:
+#                 partners |= self.env['res.partner'].browse(active_id)
+#         return partners
 
-    website_ids = fields.Many2many("website", string="To Show on Website", help="IF blank, the product variant will be visible in all websites")
+    website_ids = fields.Many2many("website", string="To Show on Website", 
+                                   help="IF blank, the product variant will be visible in all websites")
 
     @api.multi
     def is_product_variant_publish(self):
         for rec in self:
-            current_website = rec.env['website'].get_current_website()
-            if not rec.website_ids:
-                return True
-            elif rec.website_ids:
-                if current_website.id in rec.website_ids.ids:
+            if rec.website_published:
+                current_website = rec.env['website'].get_current_website()
+                if not rec.website_ids and rec.is_website_publish:
                     return True
-                else:
+                elif not rec.is_website_publish:
                     return False
+                elif rec.website_ids and rec.is_website_publish:
+                    if current_website.id in rec.website_ids.ids:
+                        return True
+                    else:
+                        return False
 
 #     @api.onchange('is_website_publish')
 #     def _onchange_is_website_publish(self):
