bb1bf68bce17ff4d53c5c7ca66e29ccaf23be9be
bista <jaykishan.sharma@bistasolutions.com>
2019-05-07 18:49:13 +0530

    [ADD/REF] Added new js code and improved the product specification view

--- maq_custom_theme/static/src/js/website.js
+++ maq_custom_theme/static/src/js/website.js
@@ -47,4 +47,43 @@ require('website_sale.website_sale');
 
     });
 
+    $('.oe_website_sale').on('click', 'tr.variant_details label[label-default="label-default"]', function (ev) {
+        var product_id = $(ev.target).closest('tr.variant_details')[0].id;
+        console.log(product_id);
+        $('.oe_website_sale').find('label.active').each(function () {
+            this.style.color = "#555555";
+            this.className = '';
+        });
+        this.style.color = "#1ad68f";
+        this.className = 'active';
+        update_product_image(this, product_id);
+    });
+
+    function update_product_image(event_source, product_id) {
+        var $img;
+        if ($('#o-carousel-product').length) {
+            $img = $(event_source).closest('tr.js_product, .oe_website_sale').find('img.js_variant_img');
+            $img.attr("src", "/web/image/product.product/" + product_id + "/image");
+            $img.parent().attr('data-oe-model', 'product.product').attr('data-oe-id', product_id)
+                .data('oe-model', 'product.product').data('oe-id', product_id);
+
+            var $thumbnail = $(event_source).closest('tr.js_product, .oe_website_sale').find('img.js_variant_img_small');
+            if ($thumbnail.length !== 0) { // if only one, thumbnails are not displayed
+                $thumbnail.attr("src", "/web/image/product.product/" + product_id + "/image/90x90");
+                $('.carousel').carousel(0);
+            }
+        }
+        else {
+            $img = $(event_source).closest('tr.js_product, .oe_website_sale').find('span[data-oe-model^="product."][data-oe-type="image"] img:first, img.product_detail_img');
+            $img.attr("src", "/web/image/product.product/" + product_id + "/image");
+            $img.parent().attr('data-oe-model', 'product.product').attr('data-oe-id', product_id)
+                .data('oe-model', 'product.product').data('oe-id', product_id);
+        }
+        // reset zooming constructs
+        $img.filter('[data-zoom-image]').attr('data-zoom-image', $img.attr('src'));
+        if ($img.data('zoomOdoo') !== undefined) {
+            $img.data('zoomOdoo').isReady = false;
+        }
+    }
+
 });
