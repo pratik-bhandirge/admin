27762b0695dfbd47e66c3aefc0b81fd661e50f39
bista <jaykishan.sharma@bistasolutions.com>
2019-05-29 17:38:54 +0530

    [IMP] PEP8 improvements

--- maq_picking_box_number/models/stock_picking_inherit.py
+++ maq_picking_box_number/models/stock_picking_inherit.py
@@ -1,13 +1,11 @@
-
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api, _
 
 
 class StockMove(models.Model):
-	_inherit = 'stock.move'
-	'''
-    Added Box number field in stock.move model
-    '''
+    _inherit = 'stock.move'
+
+    '''Added Box number field in stock.move model'''
 
     m_box_number = fields.Char(string="Box Number")
