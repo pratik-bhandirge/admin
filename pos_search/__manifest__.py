a3833053202d7211e80e276c667314473aff9c4e
Pratik Bhandirge <pratik.bhandirge@bistasolutions.com>
2019-05-29 12:37:09 +0530

    [REF] improvements in pos_search module and convert it into PEP8 format

--- pos_search/__manifest__.py
+++ pos_search/__manifest__.py
@@ -1,18 +1,19 @@
 # -*- coding: utf-8 -*-
+
 {
     'name': 'POS Search',
     'version': '1.0',
     'category': 'Point Of Sale',
-    'summary': '',
-    'description': """ """,
+    'summary': 'Improves a product search in POS',
+    'description': """This module allows user to search product in POS based on
+    - Internal Reference
+    - Product Name
+    - Product Category
+    - Barcode""",
     'author': 'Bista Solutions India Pvt Ltd.',
     'depends': ['point_of_sale'],
-    'data': [
-        'views/assets.xml'
-    ],
-    'qweb': [
-        'static/src/xml/pos_search.xml',
-    ],
+    'data': ['views/assets.xml'],
+    'qweb': ['static/src/xml/pos_search.xml'],
     'installable': True,
     'auto_install': False
 }
