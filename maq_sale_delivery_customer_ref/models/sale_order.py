74b5c4006554619ffa5dae2abde4ef5181eec243
Pratik Bhandirge <pratik.bhandirge@bistasolutions.com>
2019-05-31 13:11:30 +0530

    [REF] Rename 'External Reference' to 'External PO Reference'

--- maq_sale_delivery_customer_ref/models/sale_order.py
+++ maq_sale_delivery_customer_ref/models/sale_order.py
@@ -14,4 +14,4 @@ class StockPicking(models.Model):
     _inherit = 'stock.picking'
 
     m_delivery_customer_ref = fields.Char(
-        related='sale_id.client_order_ref', string="External Reference", track_visibility='onchange')
+        related='sale_id.client_order_ref', string="External PO Reference", track_visibility='onchange')
