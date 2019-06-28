d8aaa1b356bca4931c4822eb6687c1ad279db3a4
Prashant Panchal <ppa@odoo.com>
2019-06-28 12:31:34 +0530

    [FIX][1997780][KAD] Maqabim Distributors Ltd.: database switcher & v11 multi-website bug

---
+++ maq_multi_company_pricelist_fixed/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    "name": "Maqabim Distributors: Multi Company PriceList Fixed",
+    'summary': "PriceList",
+    'description': """
+Maqabim Distributors: Multi Company PriceList Fixed
+===================================================
+""",
+    "author": "Odoo Inc",
+    'website': "https://www.odoo.com",
+    'category': 'Custom Development',
+    'version': '0.1',
+    'depends': ['product'],
+    'data': [
+    ],
+}
