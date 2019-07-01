ffee98b28fdd07b13141dd29130f66e776ade727
pratik <pratik.bhandirge@bistasolutions.com>
2019-07-01 11:32:57 +0530

    [REF] Improvements in code

--- maqabim_purchase/models/sale.py
+++ maqabim_purchase/models/sale.py
@@ -11,3 +11,25 @@ class SaleOrderLine(models.Model):
     @api.onchange('product_uom_qty')
     def onchange_quantity(self):
         self.print_qty = int(self.product_uom_qty)
+
+    @api.model
+    def create(self, vals):
+        if vals.get('product_uom_qty') and 'print_qty' not in vals:
+            vals['print_qty'] = int(vals['product_uom_qty'])
+        res = super(SaleOrderLine, self).create(vals)
+        return res
+
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+    @api.multi
+    def _cart_update(self, product_id=None, line_id=None, add_qty=0, set_qty=0, attributes=None, **kwargs):
+        """ Add or set product quantity, add_qty can be negative """
+        order_line_vals = super(SaleOrder, self)._cart_update(product_id=product_id, line_id=line_id, add_qty=add_qty, set_qty=set_qty, attributes=attributes, **kwargs)
+        if order_line_vals.get('line_id'):
+            SaleOrderLineSudo = self.env['sale.order.line'].sudo()
+            order_line = SaleOrderLineSudo.browse(order_line_vals['line_id'])
+            if order_line.exists():
+                order_line.print_qty = order_line_vals['quantity']
+        return order_line_vals
