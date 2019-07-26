d7fa941c052c6c9fb7a1e41a4bb7587923e0f432
pratk <pratik.bhandirge@bistasolutions.com>
2019-07-26 18:48:44 +0530

    [REF] refactor code with production logic

--- maq_search_bar/controllers/main.py
+++ maq_search_bar/controllers/main.py
@@ -11,28 +11,22 @@ class WebsiteSaleCustom(WebsiteSale):
     def search_result(self, search_keyword=None):
         prod_obj = request.env['product.template']
         category_obj = request.env['product.public.category']
-        current_website = request.env['website'].get_current_website()
         if search_keyword:
-            category_ids = category_obj.sudo().search(
-                ['|', ('website_id', '=', current_website.id), ('website_id', 'in', [False, '']),
-                 ('name', 'ilike', search_keyword)], limit=8)
-            if len(category_ids) < 8:
-                if search_keyword:
-                    for srch in search_keyword.split(" "):
-                        domain = ['|', ('website_id', '=', current_website.id), ('website_id', 'in', [False, '']),
-                                  ('name', 'ilike', srch)]
-                        search_category_ids = category_obj.sudo().search(domain, limit=8)
-                        category_ids += search_category_ids
-                        if len(category_ids) >= 8:
-                            category_ids = category_ids[0:8]
-                            break
-            domain = []
-            for srch in search_keyword.split(" "):
-                domain += [
-                    '|', '|', '|', ('name', 'ilike', srch), ('description', 'ilike', srch),
-                    ('description_sale', 'ilike', srch), ('product_variant_ids.default_code', 'ilike', srch)]
-            product_ids = prod_obj.sudo().search(domain, limit=8)
-            product_count = len(product_ids)
+            current_website = request.env['website'].get_current_website()
+            product_count = prod_obj.search_count([
+                '|', ('name', 'ilike', search_keyword),
+                ('product_variant_ids.default_code', 'ilike', search_keyword),
+                ('website_published', '=', True),
+                # ('website_discontinued','=',False),
+            ])
+            product_ids = prod_obj.search([
+                '|', ('name', 'ilike', search_keyword),
+                ('product_variant_ids.default_code', 'ilike', search_keyword),
+                ('website_published', '=', True),
+                # ('website_discontinued','=',False),
+            ], limit=8)
+            category_ids = category_obj.search(
+                ['|', ('website_id', '=', current_website.id), ('website_id', 'in', [False, '']), ('name', 'ilike', search_keyword)], limit=8)
             return request.env['ir.ui.view'].render_template(
                 'maq_search_bar.website_search_result', {
                     'product_ids': product_ids,
