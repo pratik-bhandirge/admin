370c5fbb30d185dde72362ff5e94fa086325b2dc
bista <jaykishan.sharma@bistasolutions.com>
2019-08-02 21:54:04 +0530

    [ADD/IMP] Refactor the code, removed commented code. Add list_price in kanban view of product template.

--- maq_intercompany/models/__init__.py
+++ maq_intercompany/models/__init__.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 
-# from . import calendar_event
 from . import ir_rule
 from . import product_inherit
-from . import product_public_category_inherit
 from . import website_multi_intercompany
--- maq_intercompany/models/product_inherit.py
+++ maq_intercompany/models/product_inherit.py
@@ -8,15 +8,6 @@ class ProductProduct(models.Model):
 
     _inherit = 'product.product'
 
-#     price_extra = fields.Float(
-#         'Variant Price Extra', compute='_compute_product_price_extra',
-#         digits=dp.get_precision('Product Price'),
-#         help="This is the sum of the extra price of all attributes")
-#     lst_price = fields.Float(
-#         'Sale Price', compute='_compute_product_lst_price',
-#         digits=dp.get_precision('Product Price'), inverse='_set_product_lst_price',
-#         help="The sale price is managed from the product template. Click on the 'Variant Prices' button to set the extra attribute prices.")
-
     def action_view_stock_move_lines(self):
         res = super(ProductProduct, self).action_view_stock_move_lines()
         res.update({'domain': ['&', ('product_id', '=', self.id),
@@ -74,49 +65,12 @@ class ProductTemplate(models.Model):
         digits=dp.get_precision('Product Price'),
         company_dependent=True,
         help="Base price to compute the customer price. Sometimes called the catalog price.")
-    description = fields.Text(
-        'Description', translate=True, company_dependent=True,
-        help="A precise description of the Product, used only for internal information purposes.")
-    description_purchase = fields.Text(
-        'Purchase Description', translate=True, company_dependent=True,
-        help="A description of the Product that you want to communicate to your vendors. "
-             "This description will be copied to every Purchase Order, Receipt and Vendor Bill/Credit Note.")
-    description_sale = fields.Text(
-        'Sale Description', translate=True, company_dependent=True,
-        help="A description of the Product that you want to communicate to your customers. "
-             "This description will be copied to every Sales Order, Delivery Order and Customer Invoice/Credit Note")
-    description_picking = fields.Text('Description on Picking', company_dependent=True, translate=True)
-    description_pickingout = fields.Text('Description on Delivery Orders', company_dependent=True, translate=True)
-    description_pickingin = fields.Text('Description on Receptions', company_dependent=True, translate=True)
-#     website_description = fields.Html('Description for the website', company_dependent=True, sanitize_attributes=False, translate=html_translate)
     produce_delay = fields.Float(
         'Manufacturing Lead Time', default=0.0, company_dependent=True,
         help="Average delay in days to produce this product. In the case of multi-level BOM, the manufacturing lead times of the components will be added.")
     sale_delay = fields.Float(
         'Customer Lead Time', default=0, company_dependent=True,
         help="The average delay in days between the confirmation of the customer order and the delivery of the finished products. It's the time you promise to your customers.")
-#     public_categ_ids = fields.Many2many('product.public.category', string='Website Product Category', company_dependent=True,
-#                                         help="Categories can be published on the Shop page (online catalog grid) to help "
-#                                         "customers find all the items within a category. To publish them, go to the Shop page, "
-#                                         "hit Customize and turn *Product Categories* on. A product can belong to several categories.")
-#     alternative_product_ids = fields.Many2many('product.template', 'product_alternative_rel', 'src_id', 'dest_id', company_dependent=True,
-#                                                string='Alternative Products', help='Suggest more expensive alternatives to '
-#                                                'your customers (upsell strategy). Those products show up on the product page.')
-#     accessory_product_ids = fields.Many2many('product.product', 'product_accessory_rel', 'src_id', 'dest_id', company_dependent=True,
-#                                              string='Accessory Products', help='Accessories show up when the customer reviews the '
-#                                              'cart before paying (cross-sell strategy, e.g. for computers: mouse, keyboard, etc.). '
-#                                              'An algorithm figures out a list of accessories based on all the products added to cart.')
-#     optional_product_ids = fields.Many2many('product.template', 'product_optional_rel', 'src_id', 'dest_id', company_dependent=True,
-#                                             string='Optional Products', help="Optional Products are suggested "
-#                                             "whenever the customer hits *Add to Cart* (cross-sell strategy, "
-#                                             "e.g. for computers: warranty, software, etc.).")
-    inventory_availability = fields.Selection([
-        ('never', 'Sell regardless of inventory'),
-        ('always', 'Show inventory on website and prevent sales if not enough stock'),
-        ('threshold', 'Show inventory below a threshold and prevent sales if not enough stock'),
-        ('custom', 'Show product-specific notifications'),
-    ], string='Inventory Availability', company_dependent=True, help='Adds an inventory availability status on the web product page.', default='never')
-#     website_style_ids = fields.Many2many('product.style', company_dependent=True, string='Styles')
     invoice_policy = fields.Selection(
         [('order', 'Ordered quantities'),
          ('delivery', 'Delivered quantities'),
@@ -124,29 +78,6 @@ class ProductTemplate(models.Model):
         help='Ordered Quantity: Invoice based on the quantity the customer ordered.\n'
              'Delivered Quantity: Invoiced based on the quantity the vendor delivered (time or deliveries).',
         default='order')
-
-
-#     def action_view_stock_move_lines(self):
-#         '''This method inherits the default odoo method and updates the domain
-#            for the stock move lines related to the product company wise'''
-#         res = super(ProductTemplate, self).action_view_stock_move_lines()
-#         res.update({'domain': ['&',
-#                                ('product_id.product_tmpl_id', 'in', self.ids),
-#                                '|', '|', ('location_id.company_id.id', 'in',
-#                                           self.env.user.company_ids.ids),
-#                                ('location_id.company_id.id', '=', None),
-#                                '|', ('location_dest_id.company_id.id', 'in',
-#                                      self.env.user.company_ids.ids),
-#                                ('location_dest_id.company_id.id', '=', None)]})
-#         return res
-#     @api.depends('available_in_pos')
-#     def _get_pos_toggle(self):
-#         for rec in self:
-#             current_company = rec.env['res.users']._get_company()
-#             if rec.available_in_pos and current_company:
-#                 rec.m_available_pos = True
-#             elif not rec.available_in_pos:
-#                 rec.m_available_pos = False
 
     def action_view_routes(self):
         '''This method inherits the default odoo method and updates the domain
