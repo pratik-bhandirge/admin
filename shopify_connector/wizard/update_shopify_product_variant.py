60009bfb62cbcf6b1885abe7d4b7da598dfaa699
Pratik Bhandirge <pratik.bhandirge@bistasolutions.com>
2019-10-03 14:59:07 +0530

    [FIX] Add counter for bulk update as we can send only 2 request per second

--- shopify_connector/wizard/update_shopify_product_variant.py
+++ shopify_connector/wizard/update_shopify_product_variant.py
@@ -1,4 +1,6 @@
+# -*- coding: utf-8 -*-
 
+import time
 from odoo import models, fields, api, _
 
 
@@ -12,5 +14,10 @@ class UpdateProductVariant(models.TransientModel):
         for rec in self:
             active_ids = rec._context.get('active_ids')
             shopify_product_variant_search = shopify_product_variant_obj.search([('id', 'in', active_ids), ("shopify_product_id", "!=", False)])
+            #Add counter b'coz we can send only 2 request per second
+            count = 1
             for product in shopify_product_variant_search:
                 product.update_shopify_variant()
+                if (count % 2 == 0):
+                    time.sleep(0.5)
+                count += 1
