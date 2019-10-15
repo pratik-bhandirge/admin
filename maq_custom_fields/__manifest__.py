84db7fa0561acf3c1e1e572d73500e11fd304be8
bista <jaykishan.sharma@bistasolutions.com>
2019-10-15 16:17:28 +0530

    [ADD] Added a custom fields module to add cost price field on sale order line and vendor code field on stock move line for purchase order.

---
+++ maq_custom_fields/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+{
+    "name": "Bista Maq Custom Fields",
+    "version": "11.0.1.0.0",
+    "author": "Bista Solutions Pvt. Ltd.",
+    "maintainer": "Bista Solutions Pvt. Ltd.",
+    "website": "https://www.bistasolutions.com",
+    "category": "Custom",
+    "license": "AGPL-3",
+    'summary': 'This module adds two extra fields Cost and Vendor Code.',
+    'description': """In this module, we have added two fields in two different objects: - 
+            1) A custom cost field in sale order lines to display cost price according to company.
+            2) A vendor code field on incoming shipment for purchase order.""",
+    "depends": ['sale', 'stock'],
+    "data": [
+        "views/sale_order_view_inherit.xml",
+        "views/stock_picking_view_inherit.xml",
+    ],
+    "installable": True,
+    "application": True,
+}
