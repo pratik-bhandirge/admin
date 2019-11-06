6d90422249d88c96a4067de7071fbfdf089cc0e9
bista <jaykishan.sharma@bistasolutions.com>
2019-11-06 14:10:53 +0530

    [ADD/IMP] Added the discount product boolean field. Add unique discount product condition on create and write method. Improved the shipping product addition from shopify to odoo.

--- shopify_connector/models/product_product.py
+++ shopify_connector/models/product_product.py
@@ -10,7 +10,9 @@ class ProductProduct(models.Model):
     shopify_product_product_ids = fields.One2many(
         "shopify.product.product", "product_variant_id", "Shopify Product Variants", help="Enter Shopify Product Variants")
     shopify_shipping_product = fields.Boolean(
-        "Is Shopify Shipping Product", help="Check if this is prduct use a shipping product while import order?", track_visibility='onchange')
+        "Is Shopify Shipping Product", help="Check if this is product use a shipping product while import order?", track_visibility='onchange')
+    shopify_discount_product = fields.Boolean(
+        "Is Shopify Discount Product", help="Check if this is product use a discount product while import order?", track_visibility='onchange')
     # shopify_weight = fields.Float(
     #     "Weight", help="Enter Shopify Product Variants", track_visibility="onchange")
     # shopify_uom = fields.Many2one(
@@ -37,25 +39,35 @@ class ProductProduct(models.Model):
     @api.model
     def create(self, vals):
         """
-        Restrict a user from creating multiple shipping products for Shopify.
+        Restrict a user from creating multiple shipping products and multiple discount products for Shopify.
         """
         res = super(ProductProduct, self).create(vals)
         shopify_shipping_product = vals.get('shopify_shipping_product') or self.shopify_shipping_product
+        shopify_discount_product = vals.get('shopify_discount_product') or self.shopify_discount_product
         if shopify_shipping_product:
             shipping_product_variant_count = self.search_count([('type','=','service'),('shopify_shipping_product', '=', True)])
             if shipping_product_variant_count > 1:
                 raise ValidationError(_("Shipping Product Already Exists in the system !"))
+        if shopify_discount_product:
+            discount_product_variant_count = self.search_count([('type','=','service'),('shopify_discount_product', '=', True)])
+            if discount_product_variant_count > 1:
+                raise ValidationError(_("Discount Product Already Exists in the system !"))
         return res
 
     @api.multi
     def write(self, vals):
         """
-        Restrict a user from creating multiple shipping products for Shopify.
+        Restrict a user from creating multiple shipping products and multiple discount products for Shopify.
         """
         res = super(ProductProduct, self).write(vals)
         shopify_shipping_product = vals.get('shopify_shipping_product') or self.shopify_shipping_product
+        shopify_discount_product = vals.get('shopify_discount_product') or self.shopify_discount_product
         if shopify_shipping_product:
             shipping_product_variant_count = self.search_count([('type','=','service'),('shopify_shipping_product', '=', True)])
             if shipping_product_variant_count > 1:
                 raise ValidationError(_("Shipping Product Already Exists in the system !"))
+        if shopify_discount_product:
+            discount_product_variant_count = self.search_count([('type','=','service'),('shopify_discount_product', '=', True)])
+            if discount_product_variant_count > 1:
+                raise ValidationError(_("Discount Product Already Exists in the system !"))
         return res
